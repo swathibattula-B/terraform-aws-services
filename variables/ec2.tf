72824556f3a6e8d46fc617c8adaea8f39037d873
swathi <battulaswathi15407@gmail.com>
2026-03-31 15:12:01 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,38 +1,34 @@
-resource  "aws_instance" "my_ec2"{
-  ami = ""
-  instance_type = "t3.micro"
+resource  "aws_instance" "my_ec2" {
+  ami = "var.ami_id"
+  instance_type = "var.instance_type"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
-  tags = {
-    Name = "swathi"
-    project = "robooshop"
-  }
+  tags = var.ec2_tags
 }
 
 resource "aws_security_group" "sg"{
-  name = "allow-all-terraform"
-  description = "all_all_ports"
+  name = var.sg_name
+  description = var.sg_discpription
   
   egress {
     
-    from_port = 0
-    to_port = 0
+    from_port = var.sg_from_port
+    to_port = var.sg_to_port
     protocol ="-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.sg_cidr_blocks
     ipv6_cidr_blocks = ["::/0"]
 
   }
-  ingress{
-    from_port = 0
-    to_port = 0
+  ingress {
+    from_port = var.sg_from_port
+    to_port = var.sg_to_port
     protocol ="-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.sg_cidr_blocks
     ipv6_cidr_blocks = ["::/0"]
   }
-  tags = {
-    Name = "TERRAFORM"
+  tags = var.sg_tags
 
-  }
+  
 
   lifecycle {
     create_before_destroy = true
---
+++ variables/variables.tf
@@ -0,0 +1,56 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+    description = "RHEL image "
+
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+
+}
+variable "ec2_tags" {
+    type = map
+    default = {
+        Name = "swathi"
+        project = "robooshop"
+        env = "dev"
+
+    }
+
+} 
+
+variable "sg_name" {
+    type = string 
+    default = "allow-all-terraform"
+}
+
+variable "sg_discpription"{
+    type = string 
+    default =  "all_all_ports"
+}
+
+variable "sg_from_port" {
+    type = number
+    default = 0
+}
+
+variable "sg_to_port" {
+    type = number
+    default = 0 
+}
+
+variable "sg_cidr_blocks" {
+    type = number
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    type = map 
+    default = {
+        Name = "TERRAFORM"
+        env = "dev"
+
+    }
+}
