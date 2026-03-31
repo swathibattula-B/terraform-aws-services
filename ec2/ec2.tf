cba0c2f1afa0793d282fce2c497bf91fc1174876
swathi <battulaswathi15407@gmail.com>
2026-03-31 13:36:58 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,46 +1,40 @@
-resource "aws_instance" "my_ec2"{
-  ami = "ami-0220d79f3f480ecf5"
+resource  "aws_instance" "my_ec2"{
+  ami = ""
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
-  tags ={
-   name = "myec2"
-   project = "roboshop"
-   env= "dev"
+  tags = {
+    Name = "swathi"
+    project = "robooshop"
   }
 }
 
 resource "aws_security_group" "sg"{
-  name= "terraform_sg
-  description= "allow all terraform ports"
-
-
+  name = "allow-all-terraform"
+  description = "all_all_ports"
+  
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    
+    from_port = 0
+    to_port = 0
+    protocol ="-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+  ingress{
+    from_port = 0
+    to_port = 0
+    protocol ="-1"
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  tags = {
+    Name = "TERRAFORM"
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
-
-  tags ={
-    name= "sg"
-    project = "roboshop"
-    env = "dev"
-  }
-
-
- lifecycle {
+  lifecycle {
     create_before_destroy = true
   }
 }
-
